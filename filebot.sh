ff1f0369ad67969ac382f358604e400363eca8d7
Roma <a.romiti@open-box.it>
2015-04-29 10:51:32 +0200

    Implementation of the input parameter

--- filebot.sh
+++ filebot.sh
@@ -14,18 +14,18 @@ declare -A FILETYPES
 ################################################################################
 parseConfig ()
 {
-    COMMAND=$(cat $1 | ./vendor/shyaml/shyaml get-value command.executable)
-    COMMANDOPTIONS=$(cat $1 | ./vendor/shyaml/shyaml get-value command.options)
+    COMMAND=$(cat $1 | ./vendor/shyaml/shyaml get-value  $CONFIG.command.executable)
+    COMMANDOPTIONS=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.command.options)
 
-    FOLDERLIST=$(cat $1 | ./vendor/shyaml/shyaml get-values directories)
-    FILETYPES=$(cat $1 | ./vendor/shyaml/shyaml get-values filetypes)
-    ACTIONSFOLDER=$(cat $1 | ./vendor/shyaml/shyaml get-value actionFolder)
-    LOGDESTINATIONS=$(cat $1 | ./vendor/shyaml/shyaml get-value log.destinantions)
-    LOGFOLDER=$(cat $1 | ./vendor/shyaml/shyaml get-value log.folder)
-    LOGEMAILS=$(cat $1 | ./vendor/shyaml/shyaml get-value log.mails)
-    LOGFILEPREFIX=$(cat $1 | ./vendor/shyaml/shyaml get-value log.fileprefix)
-    LOGMAILSUBJECT=$(cat $1 | ./vendor/shyaml/shyaml get-value log.mailsubject)
-    LOGFILENAME=$(cat $1 | ./vendor/shyaml/shyaml get-value log.filename)"@"$(date "+%d-%m-%Y-%H-%M-%S")".log"
+    FOLDERLIST=$(cat $1 | ./vendor/shyaml/shyaml get-values $CONFIG.directories)
+    FILETYPES=$(cat $1 | ./vendor/shyaml/shyaml get-values $CONFIG.filetypes)
+    ACTIONSFOLDER=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.actionFolder)
+    LOGDESTINATIONS=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.log.destinantions)
+    LOGFOLDER=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.log.folder)
+    LOGEMAILS=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.log.mails)
+    LOGFILEPREFIX=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.log.fileprefix)
+    LOGMAILSUBJECT=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.log.mailsubject)
+    LOGFILENAME=$(cat $1 | ./vendor/shyaml/shyaml get-value $CONFIG.log.filename)"@"$(date "+%d-%m-%Y-%H-%M-%S")".log"
 }
 
 ################################################################################
@@ -36,6 +36,32 @@ parseActions ()
     source $1/*
 }
 
+typeConfig ()
+{
+ if [ -z "$1" ]; then
+    CONFIG='default'
+ else
+   # Checks configuration exist
+    CHECKCONFIG=$(cut -d' ' -f1 config/config.yml)
+    STRING="${CHECKCONFIG//:/ }"
+    read -a TMPARRAY <<<$STRING
+    TLEN=${#TMPARRAY[@]}
+     for (( i=0; i<${TLEN}; i++ ));
+       do
+        if [ "$1" = "${TMPARRAY[$i]}" ]; then 
+         i=$((i+1))
+         EXISTCONFIG=true
+         break
+        fi 
+       done       
+        if [ "$EXISTCONFIG" ]; then
+         CONFIG="$1"
+        else 
+         echo "Configuration "$1 "not foud. Please check config file"
+        exit
+        fi
+ fi
+}
 
 # Logger
 logMessage()
@@ -130,6 +156,7 @@ botMain ()
 ################################################################################
 # Script runner                                                                #
 ################################################################################
+typeConfig $1
 parseConfig $CONFIGFILE
 parseActions $ACTIONSFOLDER
 botMain
